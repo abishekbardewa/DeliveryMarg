60d80772d163f8134befe9b39e1f7270811bb18a
abishek <way2abishek@gmail.com>
2020-09-26 12:21:00 +0530

    Tabs Added to ProfilePage

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,7 +1,5 @@
 app.controller('profile', function ($scope, $http, $routeParams) {
 	$scope.name = 'Profile of ' + $routeParams.id;
 
-	$(document).ready(function () {
-		$('.tabs').tabs();
-	});
+	$('.tabs').tabs();
 });
